a84fa39730d35ac393ae9ef2debf78bc7c8164bc
Suthar98 <sutharvictor10@gmail.com>
2024-06-09 20:17:47 +0530

    Add project files.

---
+++ DisplayName/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace DisplayName
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(208, 69);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(310, 22);
+            this.textBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Your Name";
+            // 
+            // btnClick
+            // 
+            this.btnClick.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnClick.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClick.Location = new System.Drawing.Point(390, 125);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(128, 32);
+            this.btnClick.TabIndex = 2;
+            this.btnClick.Text = "Click Me";
+            this.btnClick.UseVisualStyleBackColor = false;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(610, 182);
+            this.Controls.Add(this.btnClick);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Student Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClick;
+    }
+}
+
